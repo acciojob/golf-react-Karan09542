40cadc2a7aa671e4c87daf492c7ee9ea0381497d
Karan09542 <karan09542@gmail.com>
2025-10-25 17:28:28 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ class App extends Component {
     render() {
         return (
             <div className="playground">
-                {this.renderBallOrButton()}
+                {this.renderChoice()}
             </div>
         )
     }
